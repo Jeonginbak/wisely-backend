4edf33860135f40d570e77daca477a3d88db3d01
Jeonginbak <jeonginflow@gmail.com>
2020-05-15 17:02:13 +0900

    testcode password 암호화해서 저장완료

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,128 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+import bcrypt
+
+from .models          import User
+
+from django.test      import TestCase
+from django.test      import Client
+from unittest.mock    import patch, MagicMock
+
+class EmailCheckTest(TestCase):
+    def setUp(self):
+        User.objects.create(
+            email = 'test@gmail.com',
+            name  = '와이즐리'
+        )
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    def test_emailCheck_post_login(self):
+        client = Client()
+        user = {'email' : 'test@gmail.com'}
+        response = client.post('/check', json.dumps(user), content_type = 'application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(),
+             {'data' :
+             {'name' : '와이즐리', 'message' : 'EMAIL_EXISTS'}})
+
+    def test_emailCheck_post_account(self):
+        client = Client()
+        user = {'email' : 'test1@gmail.com'}
+        response = client.post('/check', json.dumps(user), content_type = 'application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(),
+            {'message' : 'EMAIL_DO_NOT_EXISTS'})
+
+    def test_emailCheck_post_key_error(self):
+        client = Client()
+        user = {'emal': 'test@gmail.com'}
+        response = client.post('/check', json.dumps(user), content_type = 'application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),
+             {'message' : 'KEY_ERROR'})
+
+class SignUpTest(TestCase):
+    def setUp(self):
+         User.objects.create(
+            email         = 'test1@test.com',
+            password      =  bcrypt.hashpw('1234'.encode('utf-8'), bcrypt.gensalt()).decode('utf-8'),
+            phone         = '010-3333-4444',
+            birth         = '2020-05-14',
+            name          = '위즐리',
+            gender        = '남자',
+            alarm_confirm = 1
+        )
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    def test_signup_success(self):
+        client = Client()
+        signup = {
+            'email'         : 'test2@test.com',
+            'password'      :  bcrypt.hashpw('1234'.encode('utf-8'), bcrypt.gensalt()).decode('utf-8'),
+            'phone'         : '010-5558-5555',
+            'birth'         : '2020-05-14',
+            'name'          : '와아이',
+            'gender'        : '여자',
+            'alarm_confirm' : 1
+        }
+
+        response = client.post('/signup', json.dumps(signup), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_key_error(self):
+        client = Client()
+        signup = {
+            'emai'          : 'test2@test.com',
+            'password'      :  bcrypt.hashpw('1234'.encode('utf-8'), bcrypt.gensalt()).decode('utf-8'),
+            'phone'         : '010-5558-5555',
+            'birth'         : '2020-05-14',
+            'name'          : '와아이',
+            'gender'        : '여자',
+            'alarm_confirm' : 1
+        }
+
+        response = client.post('/signup', json.dumps(signup), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),
+             {'message' : 'KEY_ERROR'}
+        )
+
+class LoginTest(TestCase):
+    def setUp(self):
+        User.objects.create(
+        email    = 'test@test.com',
+        password = bcrypt.hashpw('1234'.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+        )
+    def tearDown(self):
+        User.objects.all().delete()
+
+    def test_login_success(self):
+        client = Client()
+        user = 'test@test.com'
+        login = {'email' : 'test@test.com', 'password' : '1234'}
+        response = client.post('/login', json.dumps(login), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(),
+        {'access_token' : response.json()['access_token']})
+
+    def test_login_fail(self):
+        client = Client()
+        user = 'test@test.com'
+        login = {'email' : 'test@test.com', 'password' : '1235'}
+        response = client.post('/login', json.dumps(login), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_login_key_error(self):
+        client = Client()
+        user = 'test@test.com'
+        login = {'mail' : 'test@test.com', 'password' : '1235'}
+        response = client.post('/login', json.dumps(login), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),
+             {'message' : 'KEY_ERROR'}
+        )
+
--- user/views.py
+++ user/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+import json
+import bcrypt
+import jwt
 
-# Create your views here.
+from django.views    import View
+from django.http     import HttpResponse, JsonResponse
+
+from .models         import User
+from wisely.settings import SECRET_KEY
+
+class EmailCheckView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        try:
+            if User.objects.filter(email = data['email']).exists():
+               user = User.objects.get(email=data['email'])
+               data = {
+                        'name'    : user.name,
+                        'message' : 'EMAIL_EXISTS'
+               }
+               return JsonResponse({'data' : data}, status = 200)
+            return JsonResponse({'message' : 'EMAIL_DO_NOT_EXISTS'}, status = 200)
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
+
+class SignUpView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        try:
+            User.objects.create(
+                email         = data['email'],
+                password      = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8'),
+                phone         = data['phone'],
+                birth         = data['birth'],
+                name          = data['name'],
+                gender        = data['gender'],
+                alarm_confirm = data['alarm_confirm']
+            )
+            return HttpResponse(status = 200)
+        except KeyError:
+           return JsonResponse({'message':'KEY_ERROR'}, status = 400)
+
+class LogInView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        try:
+            user = User.objects.get(email = data['email'])
+            if bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
+               token = jwt.encode({'id' : user.id}, SECRET_KEY, algorithm = 'HS256')
+               return JsonResponse({'access_token' : token.decode('utf-8')}, status=200)
+            return HttpResponse(status = 400)
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
+
